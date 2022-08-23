998d34236c7778189321052b3863d9958bf7df17
GarryProhor <84273694+GarryProhor@users.noreply.github.com>
2022-08-23 21:03:39 +0300

    Creating tileIsOccupied function && refactoring to class

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -1,35 +1,67 @@
-import {PieceType, TeamType} from "../components/Chessboard";
+import {PieceType, TeamType, Piece} from "../components/Chessboard";
 
-export const isValidMove = (px: number, py: number, x: number, y: number, type: PieceType, team: TeamType) =>{
-    console.log("referee is checking the move...")
-    console.log(`previos: (${px},${py}) `);
-    console.log(`current: (${x},${y}) `);
-    console.log(`type: (${type}) `);
-    console.log(`team: (${team}) `);
+export default class Referee {
 
-    if(type === PieceType.PAWN){
-        if(team === TeamType.OUR){
-            if(py === 1){
-                if(px === x && (y - py === 1 || y - py === 2)){
-                    return true;
+
+    tileIsOccupied  (x: number, y: number, boardState: Piece[]): boolean  {
+       const  piece = boardState.find((p) =>
+       p.x === x && p.y === y);
+       if(piece){
+           return true;
+       }else{
+           return false;
+       }
+
+    };
+
+
+
+    isValidMove (px: number,
+                   py: number,
+                   x: number,
+                   y: number,
+                   type: PieceType,
+                   team: TeamType,
+                   boardState: Piece[]
+    )  {
+        console.log("referee is checking the move...")
+        console.log(`previos: (${px},${py}) `);
+        console.log(`current: (${x},${y}) `);
+        console.log(`type: (${type}) `);
+        console.log(`team: (${team}) `);
+
+        if (type === PieceType.PAWN) {
+            if (team === TeamType.OUR) {
+                if (py === 1) {
+                    if (px === x && (y - py === 1 || y - py === 2)) {
+                        if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
+                    }
+                } else {
+                    if (px === x && y - py === 1) {
+                        if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
+                    }
                 }
-            }else {
-                if(px === x && y - py === 1){
-                    return true;
-                }
-            }
-        }else {
-            if(py === 6){
-                if(px === x && (y - py === -1 || y - py === -2)){
-                    return true;
-                }
-            }else{
-                if(px === x && y - py === -1){
-                    return true;
+            } else {
+                if (py === 6) {
+                    if (px === x && (y - py === -1 || y - py === -2)) {
+                        if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
+                    }
+                } else {
+                    if (px === x && y - py === -1) {
+                        if (!this.tileIsOccupied(x, y, boardState)) {
+                            return true;
+                        }
+                    }
                 }
             }
         }
-    }
 
-    return false;
+        return false;
+    }
 }
