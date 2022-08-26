7432ef6bd37bd913610fc356eb554c450f2441af
Garry <igorprohorchenko@gmail.com>
2022-08-26 12:26:56 +0300

    Rook logic

--- src/referee/Referee.ts
+++ src/referee/Referee.ts
@@ -185,6 +185,30 @@ export default class Referee {
                     }
                 }
             }
+        } else if (type === PieceType.ROOK) {
+            //    moving logic for the BISHOP
+            //horizontal
+            if (initialPosition.y === desiredPosition.y) {
+                for (let i = 1; i < 8; i++) {
+                    let multiplier = (desiredPosition.x < initialPosition.x) ? -1 : 1;
+
+                    let passedPosition: Position = {x: initialPosition.x + (i * multiplier), y: initialPosition.y};
+                    if (passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+                        break;
+                    }
+                }
+            }
+            //vertical
+            if (initialPosition.x === desiredPosition.x) {
+                for (let i = 1; i < 8; i++) {
+                    let multiplier = (desiredPosition.y < initialPosition.y) ? -1 : 1;
+
+                    let passedPosition: Position = {x: initialPosition.x, y: initialPosition.y + (i * multiplier)};
+                    if (passedPosition.x === desiredPosition.x && passedPosition.y === desiredPosition.y) {
+                        break;
+                    }
+                }
+            }
         }
         return false;
     }
